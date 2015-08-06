37e2095944630b5ca3086a7557a5e5814618801a
Lina Lu <Lina@Yaqings-MacBook-Air.local>
2015-08-05 18:38:49 -0700

    further changes to front end setup.

--- client/app/post/post.js
+++ client/app/post/post.js
@@ -1,16 +1,6 @@
 'use strict';
 
-// angular.module('shortly.links', [])
-// .controller('LinksController', function ($scope, Links) {
-//   $scope.data = {};
-//   $scope.getLinks = function() {
-//     Links.getAllLinks()
-//       .then(function (links) {
-//       $scope.data.links = links;
-//       })
-//       .catch(function (error) {
-//       console.error(error);
-//     });
-//   };
-//   $scope.getLinks();
-// });
+angular.module('reactor.post', [])
+.controller('PostController', function ($scope) {
+  
+});
--- client/app/searchResults/searchResults.js
+++ client/app/searchResults/searchResults.js
@@ -1,1 +1,6 @@
 'use strict';
+
+angular.module('reactor.searchResults', [])
+.controller('SearchResultsController', function ($scope) {
+  
+});
