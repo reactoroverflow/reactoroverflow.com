10beeb111f64f12faee948b9f6673e4867574e9d
Kevin Schweigert <Kevin@Air.local>
2015-08-05 19:37:14 -0700

    seperated posts from core

--- server/routes/core.routes.server.js
+++ server/routes/core.routes.server.js
@@ -6,11 +6,4 @@ var core = require(path.resolve('./server/controllers/core.controllers.server.js
 
 module.exports = function(app) {
   app.get('/', core.renderIndex);
-  app.get('/api/posts', core.renderPosts);
-  
-  app.post('/api/posts', core.storePost);
-
-  app.put('/api/posts', core.updatePost);
-
-  app.delete('/apit/posts', core.deletePost);
 };
