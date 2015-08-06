5b75685bd0e97f4bf1fa3915e11ce84b00e3e5c2
Lina Lu <Lina@Yaqings-MacBook-Air.local>
2015-08-05 17:00:06 -0700

    created app folder and added files inside there. added .bowerrc. Mostly just frontend setup

---
+++ client/app/post/post.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// angular.module('shortly.links', [])
+// .controller('LinksController', function ($scope, Links) {
+//   $scope.data = {};
+//   $scope.getLinks = function() {
+//     Links.getAllLinks()
+//       .then(function (links) {
+//       $scope.data.links = links;
+//       })
+//       .catch(function (error) {
+//       console.error(error);
+//     });
+//   };
+//   $scope.getLinks();
+// });
