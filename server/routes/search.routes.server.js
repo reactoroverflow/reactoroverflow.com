2c130cd0df40ba7f0c421867ac6681bdca92e7d6
Cody B. Daig <cody@daig.me>
2015-08-06 15:59:56 -0700

    Simplified Search for Now

--- server/routes/search.routes.server.js
+++ server/routes/search.routes.server.js
@@ -8,8 +8,8 @@ module.exports = function(app) {
 
   app.route('/api/search').get(search.search);
 
-  app.route('/api/search/posts').get(search.searchPosts);
+  //app.route('/api/search/posts').get(search.searchPosts);
 
-  app.route('/api/search/comments').get(search.searchComments);
+  //app.route('/api/search/comments').get(search.searchComments);
 
 };
