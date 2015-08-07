bce57cf23bcb458375c7ca0a89f3ff1139306f68
Cody B. Daig <cody@daig.me>
2015-08-07 12:39:46 -0700

    Grunt Test for jshint

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'index.js', 'server/**/*.js', 'lib/**/*.js'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        node: true,
+        globals: {
+          jQuery: true
+        },
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks 
+
+  grunt.registerTask('default test', ['jshint']);
+
+};
