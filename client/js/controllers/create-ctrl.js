fe11cc0d46385b41d35d471dcd9a882accca1df4
Lina Lu <linalu001@gmail.com>
2015-08-07 15:19:02 -0700

    posting 'works', able to grab texts from three input fields. Tags need to be put in the right format for the array.

--- client/js/controllers/create-ctrl.js
+++ client/js/controllers/create-ctrl.js
@@ -1,33 +1,26 @@
+'use strict';
+
 /**
  * Create Controller
  */
 
 angular.module('RDash')
 .controller('CreateCtrl', function CreateCtrl ($scope, Posts) {
-  console.log("------------------------>inside controller")
   $scope.title = '';
   $scope.content = '';
   $scope.tags = [];
-  $scope.post = {
-    title: $scope.title, 
-    content: $scope.content, 
-    tags: $scope.tags
-    }; //keys: title, content and tags
-  console.log("----------------------------->", $scope.post);
-
   $scope.createPost = function() {
-    console.log("----------------------------->clicked createPost");
-    console.log("----------------------------->", $scope.title);
-
-    console.log("----------------------------->", $scope.post);
+    $scope.post = {
+      title: $scope.title, 
+      content: $scope.content, 
+      tags: $scope.tags
+      }; //keys: title, content and tags
     Posts.addPost($scope.post)
     .then(function() {
       $location.path('#'); //takes user to the post they created.
     })
     .catch(function(error) {
       console.log(error);
-    })
-      
-    
-  }
+    });
+  };
 });
