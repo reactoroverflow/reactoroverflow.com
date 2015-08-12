772c2078893c758e23da8dbc51b655de4a144bd5
Andrew Kishino <AndrewKishino@gmail.com>
2015-08-12 16:33:52 -0700

    groundwork for comments to be displayed and retrieved from server

--- client/js/controllers/post-ctrl.js
+++ client/js/controllers/post-ctrl.js
@@ -5,8 +5,9 @@
  */
 
 angular.module('RDash')
-.controller('PostCtrl', function PostCtrl($scope, $stateParams, Posts) {
+.controller('PostCtrl', function PostCtrl($scope, $stateParams, $location, Posts, Comments) {
   $scope.data = {};
+  $scope.content = '';
 
   $scope.fetch = function(){
     Posts.getPost($stateParams.postID, function (resp) {
@@ -15,5 +16,27 @@ angular.module('RDash')
   };
 
   $scope.fetch();
+
+  $scope.createComment = function() {
+    $scope.content = $scope.simplemde.value();
+    $scope.comment = {
+      content: $scope.content
+      }; //keys: content
+    // console.log("$scope.comment",$scope.comment);
+    Comments.addComment($scope.comment)
+    .then(function(resp) {
+      $location.reload(); //takes user to the post they created.
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  };
+
+  $scope.$on('$viewContentLoaded', function(){
+    $scope.simplemde = new SimpleMDE({
+      tabSize: 2
+    });
+    $scope.simplemde.render();
+  });
 
 });
