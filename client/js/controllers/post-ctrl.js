b66d902ac16c88dec0d0861b32e7860ced5fd453
Andrew Kishino <AndrewKishino@gmail.com>
2015-08-12 23:22:41 -0700

    Made some changes so that comments get posted to the server

--- client/js/controllers/post-ctrl.js
+++ client/js/controllers/post-ctrl.js
@@ -7,25 +7,26 @@
 angular.module('RDash')
 .controller('PostCtrl', function PostCtrl($scope, $stateParams, $location, Posts, Comments) {
   $scope.data = {};
-  $scope.content = '';
 
   $scope.fetch = function(){
     Posts.getPost($stateParams.postID, function (resp) {
       $scope.data.post = resp._source;
+    });
+    Comments.getComments(function (resp) {
+      $scope.data.comments = resp;
     });
   };
 
   $scope.fetch();
 
   $scope.createComment = function() {
-    $scope.content = $scope.simplemde.value();
     $scope.comment = {
-      content: $scope.content
-      }; //keys: content
+      content: $scope.simplemde.value()
+    }; //keys: content
     // console.log("$scope.comment",$scope.comment);
     Comments.addComment($scope.comment)
     .then(function(resp) {
-      $location.reload(); //takes user to the post they created.
+      window.location.reload();// $location.path('/post/'+resp._id); //takes user to the post they created.
     })
     .catch(function(error) {
       console.log(error);
