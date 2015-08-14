d17592ee24a783bc3e015315ae6ebaaa0510c24b
Cody B. Daig <cody@daig.me>
2015-08-14 11:11:58 -0600

    Set Swig as the View Engine

--- server/controllers/core.controllers.server.js
+++ server/controllers/core.controllers.server.js
@@ -4,5 +4,5 @@ var path = require('path');
 
 exports.renderIndex = function(req, res) {
   //serve up static index.html
-  res.sendFile(path.resolve('./client/index.html'));
+  res.render(path.resolve('./client/index'));
 };
