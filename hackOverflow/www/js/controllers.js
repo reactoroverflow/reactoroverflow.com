9869154e3c273d2a7b55cc799850fa9a344324ac
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-08-19 10:40:32 -0700

    (fix) menu link to profile links to main profile page

--- hackOverflow/www/js/controllers.js
+++ hackOverflow/www/js/controllers.js
@@ -1,7 +1,10 @@
 angular.module('hackOverflow.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, History) {
 
+  $scope.navToProfile = function() {
+    History.navToProfile();
+  };
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
