779b1a6ce5671ec7351fc098b2b1fbce1ff4c625
Kiyomi Li <kshiraiw@gmail.com>
2015-08-19 14:58:29 -0700

    (feat)Add controller for searching by tag, add landing page

--- hackOverflow/www/js/postTagCtrl.js
+++ hackOverflow/www/js/postTagCtrl.js
@@ -1,5 +1,16 @@
 angular.module('hackOverflow.postTag', [])
 
-.controller('PostTagCtrl', function($scope) {
+.controller('PostTagCtrl', function($scope, $stateParams, Posts) {
+  $scope.getPostsByTag = function() {
+    $scope.data = {};
+    Posts.getPostsByTag($stateParams.tagName, function (data) {
+      $scope.data.posts = data;
+      $scope.data.posts.forEach(function (post) {
+        post._source.created_at = new Date(post._source.created_at).toString();
+        post._source.contentNoTags = post._source.content.replace(/(<([^>]+)>)/ig, '');
+      });
+    });
+  };
 
+  $scope.getPostsByTag();
 });
