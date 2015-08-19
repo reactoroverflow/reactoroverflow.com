7d2fa08f98f367a9e2cee57248cfd8e029798db9
Kiyomi Li <kshiraiw@gmail.com>
2015-08-19 10:39:45 -0700

    Add posts services to create page

--- hackOverflow/www/js/createCtrl.js
+++ hackOverflow/www/js/createCtrl.js
@@ -1,6 +1,6 @@
 angular.module('hackOverflow.create', [])
 
-.controller('CreateCtrl', function($scope, $cordovaCamera ) {
+.controller('CreateCtrl', function($scope, $cordovaCamera, Posts) {
   $scope.takePhoto = function() {
     $cordovaCamera.getPicture({
       quality: 75,
@@ -19,19 +19,19 @@ angular.module('hackOverflow.create', [])
     });
   };
 
-  // $scope.createPost = function() {
-  //   $scope.post = {
-  //     title: $scope.post.title,
-  //     content: marked($scope.post.content),
-  //     tags: $scope.post.tags, //format: Tags: ["asdf","asdf"]
-  //     data: $scope.data
-  //     }; //keys: title, content and tags
-  //   Posts.addPost($scope.post)
-  //   .then(function(resp) {
-  //     $location.path('/post/'+resp._id); //takes user to the post they created.
-  //   })
-  //   .catch(function(error) {
-  //     console.log(error);
-  //   });
-  // };
+  $scope.createPost = function() {
+    $scope.post = {
+      title: $scope.post.title,
+      content: marked($scope.post.content),
+      tags: $scope.post.tags, //format: Tags: ["asdf","asdf"]
+      data: $scope.data
+      }; //keys: title, content and tags
+    Posts.addPost($scope.post)
+    .then(function(resp) {
+      $location.path('/post/'+resp._id); //takes user to the post they created.
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  };
 });
